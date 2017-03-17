b21227511ac34bb060a81ccceaad07efcaa0fe75
Juan Martin Corallo <jmcorallo@gmail.com>
2017-03-17 18:04:29 -0300

    Added Unity project.

---
+++ Assets/EmailSender/Demo/SendEmailExample.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendEmailExample : MonoBehaviour
+{
+    public InputField SenderEmail;
+    public InputField SenderPassword;
+    public InputField SmtpClient;
+    public InputField SmtpPort;
+    public InputField ToEmail;
+    public InputField Subject;
+    public InputField Body;
+    public InputField[] Attachments;
+    public Toggle IsHtml;
+
+    public void SendEmail()
+    {
+        EmailSender.SenderEmail = SenderEmail.text;
+        EmailSender.SenderPassword = SenderPassword.text;
+        EmailSender.SmtpClient = SmtpClient.text;
+        // TODO check if port is valid
+        EmailSender.SmtpPort = int.Parse(SmtpPort.text);
+
+        List<string> atts = new List<string>();
+        foreach (InputField field in Attachments)
+        {
+            if (!string.IsNullOrEmpty(field.text))
+            {
+                atts.Add(field.text);
+            }
+        }
+        EmailSender.SendEmail(ToEmail.text, Subject.text, Body.text, IsHtml.isOn, atts.ToArray());
+    }
+
+    public void AddSampleAttachment(int index)
+    {
+        Attachments[index].text = System.IO.Path.Combine(Application.streamingAssetsPath, string.Format("SampleAttachment{0}.png", index));
+    }
+}
---
+++ Assets/EmailSender/Scripts/EmailSender.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class EmailSender
+{
+
+    public static string SenderEmail;
+    public static string SenderPassword;
+    public static string SmtpClient;
+    public static int SmtpPort;
+
+    public static void SendEmail(string to, string subject, string body, bool isHtml, string[] attachmentPaths,
+        Action<object, AsyncCompletedEventArgs> callback = null)
+    {
+        try
+        {
+            SmtpClient emailServer = new SmtpClient(SmtpClient, SmtpPort);
+            emailServer.EnableSsl = true;
+            emailServer.Credentials = (ICredentialsByHost) new NetworkCredential(SenderEmail, SenderPassword);
+
+            // TODO this is unsafe, we're bypassing SSL certification check
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            MailMessage message = new MailMessage(SenderEmail, to);
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = isHtml;
+
+            foreach (string path in attachmentPaths)
+            {
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    message.Attachments.Add(new Attachment(path));
+                }
+            }
+            if (callback == null)
+            {
+                callback = SampleCallback;
+            }
+            emailServer.SendCompleted += new SendCompletedEventHandler(callback);
+            emailServer.SendAsync(message, "");
+
+            Debug.Log("Email sent async...");
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error: " + ex.Message);
+            callback("", new AsyncCompletedEventArgs(ex, true, "Exception occured"));
+        }
+    }
+
+    private static void SampleCallback(object sender, AsyncCompletedEventArgs e)
+    {
+        if (e.Cancelled || e.Error != null)
+        {
+            Debug.Log("Error: " + e.Error.Message);
+        }
+        else
+        {
+            Debug.Log("Email sent successfully.");
+        }
+    }
+}
